c8fd1c6dee966f788a8c423f88db7f135770279f
jesuschueca <48988689+jesuschueca@users.noreply.github.com>
2022-05-01 22:02:00 +0200

    Add files via upload

--- leaderboard.js
+++ leaderboard.js
@@ -1,24 +1,42 @@
-function millisToMinutesAndSeconds(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
-firebase.database().ref('Data/01_RaceInfo').once('value', function (data) {
-    raceInfo = data.val()
-})
-firebase.database().ref('Data/02_Participants').once('value', function (data) {
-    participants = data.val()
-    // console.log(participants[1])
-})
-cell=[]
-cell.length = 2412;
-
-var speed;
+// DECLARACION DE VARIABLES
+var Timer1 = setInterval(firstRunner1,1000);
+var Timer = setInterval(Time,500);
+var reader1_Timings = []
+var reader2_Timings = []
+var reader3_Timings = []
+var raceStartTime
+var finish2 = 0
+var finish3 = 0
 var firstRunner
 var secondRunner
 var thirdRunner
 var finish2
 var finish3
+var speed
+var time
+
+// FUNCION PARA PASAR DE MILISEGUNDOS A MINUTOS Y SEGUNDOS
+function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+// OBTENER LOS DATOS DE LA CARRERA
+firebase.database().ref('Data/01_RaceInfo').once('value', function (data) {
+    raceInfo = data.val()
+})
+
+// OBTENER LOS DATOS DE LOS PARTICIPANTES
+firebase.database().ref('Data/02_Participants').once('value', function (data) {
+    participants = data.val()
+})
+
+//
+cell=[]
+cell.length = 2412;
+
+// RECUPERA LA INFORMACIÓN DE LA PANTALLA DE "TRACK"
 if (window.sessionStorage.getItem("dot_place")) {
     var dot_place = JSON.parse(window.sessionStorage.getItem("dot_place"));
     firstRunner = dot_place[1]
@@ -27,7 +45,6 @@ if (window.sessionStorage.getItem("dot_place")) {
     finish2 = dot_place[4]
     finish3 = dot_place[5]
     speed = dot_place[6]
-    // console.log(dot_place)
 }
 else {
     firstRunner = 0;
@@ -37,24 +54,23 @@ else {
     finish3 = 0
     speed = 1
 }
-var reader1_Timings = []
-var reader2_Timings = []
-var reader3_Timings = []
-var time
-var raceStartTime
+
+//
 function getData() {
     raceStartTime = new Date().getTime()
     window.sessionStorage.setItem("raceStartTime", JSON.stringify(raceStartTime));
     document.getElementById("button").setAttribute("class", "hidden");
-    // getFirebaseDataon()
 }
+
+// LLAMA A LA FUNCION PARA OBTENER LOS DATOS DE FIREBASE
 getFirebaseDataon()
-// console.log(window.sessionStorage.getItem("raceStartTime"))
+
 if (window.sessionStorage.getItem("raceStartTime")) {
-    raceStartTime = window.sessionStorage.getItem
-        ("raceStartTime")
+    raceStartTime = window.sessionStorage.getItem("raceStartTime")
     document.getElementById("button").setAttribute("class", "hidden");
 }
+
+// FUNCION PARA CALCULAR EL TIEMPO QUE DESDE EL COMIENZO DE LA CARRERA
 function Time() {
     now = new Date().getTime();
     if (raceStartTime) {
@@ -64,10 +80,11 @@ function Time() {
         time = 0
     }
 }
-var Timer = setInterval(Time,
-    500);
+
+// LLAMA A LA FUNCION PARA OBTENER EL TIEMPO ACTUAL
 Time()
 
+// FUNCION QUE RECUPERA LA INFORMACION DE FIREBASE
 function getFirebaseDataon() {
     firebase.database().ref('Data/03_Inbox').on('value', function (data) {
         keys = []
@@ -80,17 +97,16 @@ function getFirebaseDataon() {
         pName = []
         pClub = []
         runnerdelays = []
-
         inbox = data.val()
+
+        //
         Object.keys(inbox).forEach((key) => {
             keys.push(key)
             if (inbox[key].includes("Reader 3")) {
-                reader3Inbox.push(inbox[key]); // 'Bob', 47
-
+                reader3Inbox.push(inbox[key]);
             }
             if (inbox[key].includes("Reader 2")) {
                 reader2Inbox.push(inbox[key]);
-
             }
             if (inbox[key].includes("Reader 1")) {
                 reader1Inbox.push(inbox[key]);
@@ -98,41 +114,37 @@ function getFirebaseDataon() {
             if (inbox[key].includes("Reader 0")) {
                 reader0.push(inbox[key]);
             }
-
-
-
         });
-        // console.log(keys)
-        // console.log(reader0)
-
-        // console.log(reader2Inbox)
-        // console.log(reader3Inbox)
-
     });
 }
+
+//
 function firstRunner1() {
-
     var reader1_Timings = []
-    var reader3_Timings = []
     var reader2_Timings = []
-
+    var reader3_Timings = []
     var readerPositions = []
+    runnerdelays = []
     pName = []
     pClub = []
+
+    //
     arduino0 = parseInt(reader0[0].slice(22)) * 1000 + parseInt(reader0[0].slice(36))
     reader0sync = arduino0 + new Date("Mar 31, 1900").getTime();
-    arduino3 = parseInt(reader3Inbox[14].slice(22)) * 1000 + parseInt(reader3Inbox[14].slice(36)) //change this
+
+    //
+    arduino3 = parseInt(reader3Inbox[0].slice(22)) * 1000 + parseInt(reader3Inbox[0].slice(36)) //change this
     reader3sync = arduino3 + new Date("Mar 31, 1900").getTime();
-    for (i = 0; i <= reader3Inbox.length - 2; i++) { //Change This
+    for (i = 1; i <= reader3Inbox.length - 1; i++) {
         let t = reader3Inbox[i].slice(21, 31)
         let ms = Number(t.split(':')[0]) * 60 * 60 * 1000 + Number(t.split(':')[1]) * 60 * 1000 + Number(t.split(':')[2]) * 1000 + Number(t.split(':')[3]);
         reader3_Timings.push(ms);
         if (time > reader3_Timings[i]) {
             readerPositions.push(parseInt(reader3Inbox[i].slice(-2)))
         }
-        // console.log(reader1Inbox[i])
     }
 
+    //
     arduino2 = parseInt(reader2Inbox[0].slice(22)) * 1000 + parseInt(reader2Inbox[0].slice(36))
     reader2sync = arduino2 + new Date("Mar 31, 1900").getTime();
     for (i = 1; i <= reader2Inbox.length - 1; i++) {
@@ -142,25 +154,22 @@ function firstRunner1() {
         if (time > reader2_Timings[i - 1]) {
             readerPositions.push(parseInt(reader2Inbox[i].slice(-2)))
         }
-        // console.log(reader1Inbox[i])
     }
 
+    //
     arduino1 = parseInt(reader1Inbox[0].slice(22)) * 1000 + parseInt(reader1Inbox[0].slice(36))
     reader1sync = arduino1 + new Date("Mar 31, 1900").getTime();
     for (i = 1; i <= reader1Inbox.length - 1; i++) {
         let t = reader1Inbox[i].slice(21, 32)
         let ms = Number(t.split(':')[0]) * 60 * 60 * 1000 + Number(t.split(':')[1]) * 60 * 1000 + Number(t.split(':')[2]) * 1000 + Number(t.split(':')[3]) - arduino3 + arduino1;
         reader1_Timings.push(ms);
-        // console.log(time)
-        // console.log(reader1_Timings[])
         if (time > reader1_Timings[i - 1]) {
-            // console.log("reader1")
             readerPositions.push(parseInt(reader1Inbox[i].slice(-2)))
         }
-        // console.log(reader1Inbox[i])
     }
+
+    //
     if (reader1_Timings[0]) {
-        // console.log(reader1_Timings[0])
         for (i = 1; i < participants.length; i++) {
             readerPositions.push(i)
         }
@@ -168,13 +177,13 @@ function firstRunner1() {
         readerPositions.forEach((c) => {
             if (!uniquePositions.includes(c)) {
                 uniquePositions.push(c);
-                pName.push(participants[c]["Name"] + " " + participants[c]["Surname"])
-                pClub.push(participants[c]["Club"])
+                pName.push(participants[c]["Nombre"] + " " + participants[c]["Apellido"])
+                pClub.push(participants[c]["Equipo"])
             }
         }
         )
-        // console.log(uniquePositions)
-        // console.log(pName)
+
+        // CREA LOS ELEMENTOS PARA LA CLASIFICACION
         document.getElementById("leaderboard").innerHTML = ""
         const div = document.createElement("div");
         const innerdiv = document.createElement("div");
@@ -192,12 +201,13 @@ function firstRunner1() {
         name.appendChild(club1);
         club1.innerHTML = pClub[0]
 
+        // MUESTRA EL TIEMPO DE LA CARRERA SI TODAVIA NO HA ACABADO
         if (time <= reader3_Timings[0]) {
             time1.innerHTML = millisToMinutesAndSeconds(time);
         }
-        // console.log(raceStartTime)
+
+        // AÑADE UNA BANDERA SI EL PARTICIPANTE HA ACABADO LA CARRERA
         if (time >= reader3_Timings[0]) {
-            // time = reader3_Timings[0];
             const img = document.createElement("img");
             img.setAttribute("class", "flag");
             img.setAttribute("src", "finish flag.png");
@@ -205,8 +215,7 @@ function firstRunner1() {
             time1.appendChild(img)
         }
 
-
-        runnerdelays = []
+        // CALCULA LA DIFERENCIA DE TIEMPOS CON EL PRIMER CLASIFICADO
         runnerdelays.push(raceStartTime)
         for (i = 1; i < reader1_Timings.length; i++) {
             if (time >= reader3_Timings[i]) {
@@ -223,7 +232,7 @@ function firstRunner1() {
             }
         }
 
-
+        // AÑADE EL RESTO DE PARTICIPANTES A LA CLASICIACION
         for (i = 1; i < participants.length - 1; i++) {
             const div = document.createElement("div");
             const innerdiv = document.createElement("div");
@@ -244,44 +253,30 @@ function firstRunner1() {
                 img.setAttribute("src", "finish flag.png");
                 time1.appendChild(img)
             }
+
+            //LOS 3 PRIMEROS PARTICIPANTES TIENE ADEMÁS EL CLUB AL QUE PERTENECEN
             if (i < 3) {
                 const club = document.createElement("div");
                 club.innerHTML = pClub[i]
                 document.getElementById("leaderboard").appendChild(div).childNodes[0].childNodes[1].appendChild(club);
             }
-            // document.getElementById("leaderboard");
         }
     }
 }
-var Timer1 = setInterval(firstRunner1,
-    1000);
+
+// LLAMA A LA FUNCION
 firstRunner1()
-// console.log(millisToMinutesAndSeconds(runnerdelays))
 
-
-
-// console.log(reader0sync)
-// console.log(reader1sync)
-// console.log(reader2sync)
-// console.log(reader3sync)
-// console.log(raceStartTime)
-
-
-
-// console.log(reader1_Timings)
-// console.log(reader2_Timings)
-// console.log(reader3_Timings)
-var finish2 = 0
-var finish3 = 0
-
+// LLAMA A LA FUNCION
 getFirebaseDataon()
+
+//
 function fRunner() {
     var y = 2412 / 3
     var time1Coordinate = 681
     if (raceStartTime) {
         if (firstRunner1 < checkPoints[1] * y + 9 && time <= reader1_Timings[0]) {
             firstRunner1 += 1;
-            console.log("hello")
         }
         if (firstRunner1 < checkPoints[1] * y && time >= reader1_Timings[0]) {
             firstRunner1 = checkPoints[1] * y
@@ -298,8 +293,6 @@ function fRunner() {
         }
         if (time >= reader3_Timings[0]) {
             firstRunner1 = cell.length - 1
-            // console.log(time)
-            // console.log(reader3_Timings)
         }
 
         if (secondRunner1 <= checkPoints[1] * y) {
@@ -310,7 +303,6 @@ function fRunner() {
         }
         if (secondRunner1 <= checkPoints[2] * y && time >= reader1_Timings[1]) {
             secondRunner1 += 1.8
-            // console.log(Math.floor(runnerdelays[1] / time1Coordinate));
             if (time >= reader2_Timings[1]) {
                 secondRunner1 = checkPoints[2] * y
             }
@@ -352,8 +344,11 @@ function fRunner() {
         window.sessionStorage.setItem("dot_place", JSON.stringify(dot_place));
     }
 }
-// const render1 = setInterval(fRunner, 6230 / speed)
+
+//
 const render1 = setInterval(fRunner, (600000 / ((cell.length - 1) / 3)) / speed)
+
+// 
 if (time >= reader3_Timings[participants.length - 2]) { //change this
     clearInterval(render1)
     clearInterval(Timer);
